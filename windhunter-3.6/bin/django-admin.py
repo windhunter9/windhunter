f7fe13d2fb9e94eb5484cf9aa66ef1a5bfac77c1
windhunter9 <spotify-1984@web.de>
2018-12-10 16:07:26 +0100

    bla

--- windhunter-3.6/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/media/data/owncloud/code/julian_magnus/J_and_M/git/windhunter/windhunter-3.6/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()

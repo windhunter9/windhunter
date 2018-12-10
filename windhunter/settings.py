9adc2a89cc7cba77019adcaaa2ff1cbfc2e6b931
windhunter9 <spotify-1984@web.de>
2018-12-10 16:32:02 +0100

    initial commit

--- windhunter/settings.py
+++ windhunter/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -138,3 +138,5 @@ STATICFILES_DIRS = (
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+django_heroku.settings(locals())
